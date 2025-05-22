f5bb2c85dfb2dd748c29fce766893c5a5a922ec1
IrisZ <iridazervou@gmail.com>
2025-05-22 09:41:07 +0200

    wip

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  let { children } = $props();
+  import "../app.css";
+</script>
+<style>
+  :global(body) {
+    font-family: 'Alegreya Sans', sans-serif;
+  }
+</style>
+{@render children()}
---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+  import logo from '$lib/images/LogoOnly.png';
+  import menu from '$lib/images/bards-solid.svg';
+  import image from '$lib/images/1024x768_attractions-image-wot5dkgorkc6atuknj5qyq 1.png'
+  import old_street from '$lib/images/p077sqgy 2.png'
+</script>
+
+<div class="flex mt-2 pt-2 ml-2">
+<img class="w-8" src={logo} alt="Logo" />
+<h1 class="self-center font-semibold text-2xl">Phoenic of Sanxia</h1>
+<img class="absolute top-4 right-4 w-6 pt-2" src="{menu}" alt="menu"/>
+</div>
+<div class="relative">
+    <img class="brightness-50 mt-6" src="{image}" alt="title"/>
+    <div class="absolute top-55 right-32">
+    <h1 class="text-white text-7xl">Welcome to Sanxia</h1>
+    <hr class="my-12 h-px bg-white border-0"/>
+    <p class="text-white text-lg w-120 mt-8">Sanxia District,
+         located in the southwestern part of New Taipei City,
+         Taiwan, is a charming blend of rich history,
+          vibrant culture, and natural beauty.</p>
+</div>
+</div>
+<div class="my-15 ml-15">
+    <h2 class="text-3xl">Sanxia old street</h2>
+    <img class="absolute right-20 top-175 w-110" src="{old_street}" alt="old_street">
+    <p class="mt-15 mr-155">Sanxia Old Street refers to the south section of Minquan Street in New Taipei City.
+         Its length is about two hundred meters, and its architecture dates back
+          to the early days when the Republic of China was newly established.
+           Walking along Sanxia Old Street is like walking into a time tunnel;
+            the arched red brick hallways, the traditional architecture,
+             the beams, columns, ancient wooden plaques, the squat maidens walls
+              and the figure carvings upon the buildings are all very unique.
+               Walking along this ancient street brings a feeling of nostalgia
+                for the good old days, and makes visitors want to linger.</p>
+                <p class="mt-18">During the time of Sanxia's heyday, the street was lined with shops that sold dyes, manufacturing materials,
+                     and tea, and western-style houses were also being built quickly.
+                      Nowadays, only the red brick buildings with arched hallways and Baroque
+                       styled architecture remain to tell of their past glory.</p>
+</div>
+<div>
+    <h2>.</h2>
+    <p></p>
+</div>
